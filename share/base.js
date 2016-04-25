83d5e32108470430b59763f02f2b5dac6030ae73
Dylan Piercey <dylan@tillerdigital.ca>
2016-04-25 15:59:45 -0600

    Automatically ensure context type for wrapped renders

--- share/base.js
+++ share/base.js
@@ -12,17 +12,32 @@ module.exports = React.createClass({
     return { locals: this.props.locals }
   },
   render: function () {
-    var view = this.props.view
-    var type = view.type
-
-    // Ensure context types exist.
-    type.contextTypes = type.contextTypes || {}
-
-    // Check if we should automatically add locals for ease of use.
-    if (!('locals' in type.contextTypes)) {
-      type.contextTypes.locals = object
-    }
-
-    return React.cloneElement(view)
+    return ensureContextType(this.props.view)
   }
 })
+
+/**
+ * Recursively iterates over a React Element and it's children to ensure that it accepts a contextType.
+ *
+ * @param {React.Element} el
+ * @return {React.Element}
+ */
+function ensureContextType (el) {
+  if (!el) return
+  if (!el.type) return
+
+  // Ensure child context types.
+  React.Children.forEach(el.props.children, ensureContextType)
+
+  // Ensure context types exist.
+  el.type.contextTypes = el.type.contextTypes || {}
+
+  // Check if we should automatically add locals.
+  if (!('locals' in el.type.contextTypes)) {
+    el.type.contextTypes.locals = object
+    // Clone to element to ensure context type gets updated.
+    return React.cloneElement(el)
+  }
+
+  return el
+}
